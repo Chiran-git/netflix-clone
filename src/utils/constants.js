9b258d0f9886db301c0cf9220c2ff5a3b5836464
Chiranjeeb <gogoi.chiranjeeb@rediffmail.com>
2025-09-29 11:05:42 +0530

    perf: add caching and fix environment variable naming for API keys

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,8 +4,8 @@ export const API_OPTIONS = {
     method: 'GET',
     headers: {
         accept: 'application/json',
-        Authorization: 'Bearer' + process.env.TMDB_KEY
+        Authorization: 'Bearer ' + process.env.REACT_APP_TMDB_KEY
     }
 };
 
-export const OPENAI_KEY= process.env.OPENAI_KEY
+export const OPENAI_KEY= process.env.REACT_APP_OPENAI_KEY
